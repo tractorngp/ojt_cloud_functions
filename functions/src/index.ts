136bf4c00179c4c8ce04bb540531d9cded69249a
Krishna Rohit <rohit.krish1993@gmail.com>
2020-05-12 01:12:08 +0530

    node mailer user registration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,21 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as bcrypt from 'bcrypt';
+import * as nodemailer from 'nodemailer';
 
 admin.initializeApp();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 const db = admin.firestore();
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'tractorpgapp@gmail.com',
+        pass: 'tractorpgapp2020'
+    }
+});
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
@@ -93,3 +103,45 @@ export const assignRecordIdToOJT = functions.firestore.document('assigned_ojts/{
                 console.error(error, 'Error adding record Id');
             });
 });
+
+export const sendEmailToUser = functions.firestore.document('users/{doc_id}')
+        .onCreate(async (snapshot, context) => {
+            console.log("Creating User");
+            let hashVal: any;
+            const email = snapshot.get('email');
+            const pw = snapshot.get('hpw');
+            const tokenID = snapshot.get('tokenId');
+            console.log("Email: " + email);
+            bcrypt.hash(pw, "$2b$10$naep/GGixFkQDlpnBuEJAO").then(res => {
+                hashVal = res;
+                console.log("No Errors")
+                snapshot.ref.update({
+                    hpw: hashVal
+                }).then(_ => {
+                    console.log('User added successfully');
+                }).catch(error => {
+                    console.error(error, 'Error adding User');
+                });
+            }).catch(err => {
+                console.log(err);
+            });
+
+
+            const mailOptions = {
+                from: 'Tractor NGP <tractorpgapp@gmail.com>',
+                to: email,
+                subject: 'Tractor NGP OJT app registration', // email subject
+                html: `<p style="font-size: 16px;">User ID:` + tokenID + `</p> <br />
+                Password: `+ pw +`
+                ` // email content in HTML
+            };
+      
+            // returning result
+            transporter.sendMail(mailOptions, (err: any, info: any) => {
+                if(err){
+                    console.log(err)
+                }
+                console.log(info);
+            });
+});
+   
