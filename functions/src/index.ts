1edc89819cab102337de1aa18875ab26e7e164a0
Krishna Rohit <rohit.krish1993@gmail.com>
2020-06-10 23:16:17 +0530

    update password fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -141,47 +141,61 @@ export const assignRecordIdToOJT = functions.firestore.document('assigned_ojts/{
 });
 
 export const updatePassword = functions.https.onCall(async (data, context) => {
-    console.log("Creating User");
+    console.log("Updating Password");
     let hashVal: any;
     const tokenId = data.tokenId;
     const currPass = data.currentPassword;
     const newPass = data.newPassword;
-    db.collection('users').doc(tokenId).onSnapshot(async (snapshot) => {
-        const user = snapshot.data();
-        let sameVal = false;
-        let errVal: any;
-        bcrypt.compare(currPass, user?.hpw, function(err, same){
-            errVal = err;
-            sameVal = same;
-        }).then(result => {
-            if(errVal){
-                return {err: "Error comparing"}
-            }
-            else if(sameVal){
-                bcrypt.hash(newPass, "$2b$10$naep/GGixFkQDlpnBuEJAO").then(res => {
-                    hashVal = res;
-                    console.log("No Errors")
-                    snapshot.ref.update({
-                        hpw: hashVal,
-                        resetPassword: false
-                    }).then(_ => {
-                        console.log('User updated successfully');
-                        return {success: "Update success"}
-                    }).catch(error => {
-                        console.error(error, 'Error updating User');
-                    });
-                }).catch(err1 => {
-                    console.log(err1);
+    console.log("TokenID: " + tokenId)
+    console.log("Current Pass: " + currPass)
+    console.log("New Pass: " + newPass)
+    let user = await db.collection('users').doc(tokenId).get().then(function(snapshot){
+        return snapshot.data()
+    })
+
+    const userRef = db.collection('users').doc(tokenId);
+    let resFinal
+    
+    resFinal = await bcrypt.compare(currPass, user?.hpw).then(async (result) => {
+        console.log("Passwords match: " + result)
+        if(result){
+            let rF = await bcrypt.hash(newPass, "$2b$10$naep/GGixFkQDlpnBuEJAO").then(async (res) => {
+                hashVal = res;
+                console.log("No Errors")
+                let updated = await userRef.update({
+                    hpw: hashVal,
+                    resetPassword: false
+                }).then(_ => {
+                    console.log("Return 2")
+                    console.log('User updated successfully');
+                    return true
+                }).catch(error => {
+                    console.log("Return 3")
+                    console.error(error, 'Error updating User');
+                    return false
                 });
-            }
-            else{
-                return {err: "Error updating"}
-            }
-            return {result: result}
-        }).catch(err => {
-            console.log(err);
-        });
+                return updated
+            }).catch(err1 => {
+                console.log("Return 4")
+                console.log(err1);
+                return false
+            });
+            return rF;
+        }
+        else{
+            console.log("Return 5")
+            console.log()
+            return false
+        }
+    }).catch(err => {
+        console.log("Return 7")
+        console.log(err);
+        return false
     });
+    console.log("Final: ")
+    console.log(resFinal)
+    return {data: resFinal}
+    
 });
 
 export const sendEmailToUser = functions.firestore.document('users/{doc_id}')
